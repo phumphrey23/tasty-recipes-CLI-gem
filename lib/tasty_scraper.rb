e7d2220b28a6569b9c80e79606b3792c45953b24
Precious Humphrey <phumphrey23@gmail.com>
2018-08-12 20:42:25 -0400

    refined scraper methods - recipe titles

--- lib/tasty_scraper.rb
+++ lib/tasty_scraper.rb
@@ -1,20 +1,26 @@
 require 'nokogiri'
 require 'open-uri'
+require relative './recipe.rb'
 
 class Scraper
-
-  def access_site
-     Nokogiri::HTML(open("https://tasty.co/"))
+  #get url from user search converter
+  def scrape_recipe_titles(url)
+     recipe_post = Nokogiri::HTML(open(url)).css("feed.item")
+     recipes = []
+     recipe_post.each do |post|
+       recipe = Recipe.new
+       recipe.title = post.css("h6").text
+       recipes << {recipe: recipe.title}
+     end
+     recipes
   end
 
-  def get_recipe_info
-
-  end
-
-  def create_recipe
-
+  def scrape_recipe_info(recipe_url)
+    Nokogiri::HTML(open(recipe_url))
   end
 
 end
 
-#
+#CLASSES
+#feed-item
+#ingredients-prep --> ingredients, prep
