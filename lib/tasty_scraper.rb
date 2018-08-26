5fec5b76036483468308c8fb206fcc1dd8114841
Precious Humphrey <phumphrey23@gmail.com>
2018-08-26 10:21:19 -0400

    updated scrape_recipe_info to generate array

--- lib/tasty_scraper.rb
+++ lib/tasty_scraper.rb
@@ -5,7 +5,7 @@ require_relative './recipe.rb'
 
 class Scraper
   #get url from user search converter
-  def scrape_recipe_titles(url)
+  def self.scrape_recipe_titles(url)
      recipe_posts = Nokogiri::HTML(open(url)).css(".feed-item")
      recipes = []
      recipe_posts.each do |item|
@@ -16,21 +16,19 @@ class Scraper
      puts recipes
   end
 
-  def scrape_recipe_info(recipe_url)
+  def self.scrape_recipe_info(recipe_url)
     recipe_page = Nokogiri::HTML(open(recipe_url))
     recipe = {}
-    recipe[:ingredients] = recipe_page.css(".xs-mb1").css(".xs-mt0").text.gsub(/\s+/," ")
-    recipe[:instructions] = recipe_page.css(".prep-steps").children.css(".xs-mb2").text.gsub(/\s+/," ")
-    #need to edit to seperate each li with comma
-    #iterate, push to array, join elements, seperate with comma
+    recipe[:ingredients] = recipe_page.search(".ingredients__section li").map{ |li| li.text.gsub(/\s+/," ")}
+    recipe[:instructions] = recipe_page.search(".prep-steps li").map{ |li| li.text.gsub(/\s+/," ")}
     puts recipe
     #binding.pry
   end
 
 end
 
-somestuff = Scraper.new
-somestuff.scrape_recipe_info("https://tasty.co/recipe/easy-omelette-cups")
+#somestuff = Scraper.new
+#somestuff.scrape_recipe_info("https://tasty.co/recipe/easy-omelette-cups")
 
 #CLASSES
 #feed-item
