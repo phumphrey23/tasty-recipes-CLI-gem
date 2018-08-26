75318c1ac3b425a2c44e67d6c0f4e995039b5433
Precious Humphrey <phumphrey23@gmail.com>
2018-08-26 10:52:17 -0400

    added more functionality to call and recipe_info method to work on

--- lib/tasty_recipes/interface.rb
+++ lib/tasty_recipes/interface.rb
@@ -10,14 +10,17 @@ class Interface
     puts "Enter 1-3 ingredients you have"
     #Welcome message
 
-    user_input = gets.chomp.downcase
+    ingredients = gets.chomp.downcase
     #user input of ingredients
 
-    search_by_ingredients(user_input)
+    search_by_ingredients(ingredients)
     #puts recipe titles that contain input ingredients
 
-
+    puts "Enter the number of the recipe you would like to try"
+    selected_recipe = gets.chomp
     #user input of recipe to try (scrape https://tasty.co/recipe/title-of-recipe for ingredients & instructions)
+
+    recipe_info(selected_recipe)
     #puts all ingredients and instructions for selected recipe
 
 
@@ -38,14 +41,18 @@ class Interface
     end
   end
 
-  def search_by_ingredients(user_input)
+  def search_by_ingredients(ingredients)
   #generate url slug given user input for ingredients
   #scrapes generated url for titles
     base = "https://tasty.co/search?q="
-    slug = user_input.gsub(/\s/,'+')
+    slug = ingredients.gsub(/\s/,'+')
     url = base + slug
     recipe_titles = Scraper.scrape_recipe_titles(url)
     recipe_titles.format_lists
+  end
+
+  def recipe_info(recipe_chosen)
+
   end
 
 end
