0a276061ca1a3ea51db271dc81f44cbe6df114b4
Precious Humphrey <phumphrey23@gmail.com>
2018-08-26 18:44:59 -0400

    updated recipe_info method

--- lib/tasty_recipes/interface.rb
+++ lib/tasty_recipes/interface.rb
@@ -3,11 +3,11 @@ require_relative "../lib/tasty_recipes.rb"
 require_relative "../lib/interface.rb"
 
 
-class Interface
+class TastyRecipes::Interface
 
   def call
     puts "Welcome to Tasty Recipes"
-    puts "Enter 1-3 ingredients you have"
+    puts "Enter 1-3 ingredients you have or want to cook with"
     #Welcome message
 
     ingredients = gets.chomp.downcase
@@ -17,10 +17,10 @@ class Interface
     #puts recipe titles that contain input ingredients
 
     puts "Enter the number of the recipe you would like to try"
-    selected_recipe = gets.chomp
+    selected_recipe_num = gets.chomp
     #user input of recipe to try (scrape https://tasty.co/recipe/title-of-recipe for ingredients & instructions)
 
-    recipe_info(selected_recipe)
+    recipe_info(selected_recipe_num)
     #puts all ingredients and instructions for selected recipe
 
 
@@ -43,16 +43,30 @@ class Interface
 
   def search_by_ingredients(ingredients)
   #generate url slug given user input for ingredients
-  #scrapes generated url for titles
     base = "https://tasty.co/search?q="
     slug = ingredients.gsub(/\s/,'+')
     url = base + slug
+
+    #scrapes generated url for recipe titles
     recipe_titles = Scraper.scrape_recipe_titles(url)
-    recipe_titles.format_lists
+    @recipes = recipe_titles.format_lists
   end
 
-  def recipe_info(recipe_chosen)
+  def recipe_info(selected_recipe_num)
+    #generate recipe_url from selected recipe
+    if selected_recipe_num.to_i > 0
+      selected_recipe = @recipes[selected_recipe_num.to_i-1]
+    else
+      puts "please enter the number of the recipe you'd like to try"
+    end
 
+    recipe_url = "https://tasty.co/search?q=" + selected_recipe.gsub(/\s/,'-')
+
+    #scrape ingredients and instruction to add to a recipe
+    Recipe.all.each do |recipe|
+      info = Scraper.scrape_recipe_info(recipe_url)
+      recipe.add_recipe_info(info)
+    end
   end
 
 end
