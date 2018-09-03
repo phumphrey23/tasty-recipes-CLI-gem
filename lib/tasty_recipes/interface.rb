740aa9757beffe3fe71cb01615640c1ca779e8db
Precious Humphrey <phumphrey23@gmail.com>
2018-09-03 14:53:56 -0400

    Debugging problem - Titles within titles

--- lib/tasty_recipes/interface.rb
+++ lib/tasty_recipes/interface.rb
@@ -3,13 +3,11 @@
 class TastyRecipes::Interface
 
   def call
-    puts "Welcome to Tasty Recipes"
     puts "Please enter a valid search.  You can search by ingredients, meal course, or a name of a specific dish."
     #Welcome message
 
     input = gets.chomp.downcase
     #user input of ingredients
-
     search(input)
 
     #puts recipe titles that contain input ingredients
@@ -22,29 +20,12 @@ class TastyRecipes::Interface
     recipe_info(selected_recipe_num)
     #puts all ingredients and instructions for selected recipe
 
-    if recipe.ingredients != [] && recipe.instructions != []
-      print_recipe(recipe)
-    else
-      TastyRecipes::Recipe.all.clear
-      TastyRecipes::Scraper.scrape_recipe(recipe.url)
-      puts "Enter the number of the recipe you'd like to try from this compilation"
-      print_titles
-    end
-    #1 print ingredients and instruction (FINISHED)
-    #2 if compilation --> scrape_compilation_recipe, get input, get recipe_info (FINISHED)
-
-      #ELSE
-    #user input of ingredients not found
-    #error with prompt to enter less ingredients
-      #ELSE
-    #user inputs 1 ingredient and not found or user inputs no ingredients
-    #puts top 10 most recent recipe titles
 
     #PROBLEMS TO ADDRESS
-      #invalid input (special characters or blank)
+      #invalid input (special characters or blank) / RESOLVED
       #search not found
       #every title past 20 has no usable info / RESOLVED
-      #recipes within a recipe's title
+      #recipes within a recipe's title / RESOLVED
       #searching non-ingredients but still valid (ex. valid searches like breakfast, ravioli, etc will return titles) / RESOLVED
 
   end
@@ -92,7 +73,19 @@ class TastyRecipes::Interface
     #scrape ingredients and instruction to add to an instance of a recipe
       TastyRecipes::Scraper.scrape_recipe_info(selected_recipe)
 
-    #binding.pry
+    #1 print ingredients and instruction (FINISHED)
+    #2 if compilation --> scrape_compilation_recipe, get input, get recipe_info (FINISHED)
+      if selected_recipe.ingredients != [] && selected_recipe.instructions != []
+        print_recipe(selected_recipe)
+      else
+        TastyRecipes::Recipe.all.clear
+        TastyRecipes::Scraper.scrape_recipe(selected_recipe.url)
+        puts "Enter the number of the recipe you'd like to try from this compilation"
+        print_titles
+        puts "Enter the number of the recipe you would like to try"
+        selected_recipe_num = gets.chomp
+        recipe_info(selected_recipe_num)
+      end
   end
 
   def print_titles
